a310a0f01663884a686cbd6ffb0d3c28d91f8f5c
Barry Williams <barry.williams@linn.co.uk>
2014-07-07 16:38:30 +0100

    Icon changes, use template to define job alerts

--- scripts/app.js
+++ scripts/app.js
@@ -1,78 +1,88 @@
-/* Exclude Unwanted Jobs by Name */
 
-var blacklistedJobs = [ "LinnRecords (13) - Deploy All", "LinnRecords (14) - Nightly" ];
+var init = function (config) {
 
-/* TODO - Tidy this up using a templating engine */
+	var jobTemplate = _.template('<a class="job <%- type %>" href="<%- jobUrl %>"><h2 class="col-xs-12 alert alert-<%- alertState %>"><div class="col-xs-1"><span class="<%- iconCss %>"></span></div><div class="col-xs-11 name"><%- jobName %></div></h2></a>');
 
-var addProblemJob = function (job) {
-	var html = $.parseHTML('<a class="problem" href="'+job.url+'"><h2 class="col-xs-12 alert alert-danger"><span class="col-xs-1 glyphicon glyphicon-remove"></span> <div class="col-xs-11 name">'+job.name+'</div></h2></a>');
-	$(document).find('.problems').append(html);
-};
-var addRunningJob = function (job) {
-	var html = $.parseHTML('<a class="run" href="'+job.url+'"><h2 class="col-xs-12 alert alert-info"><span class="objblink col-xs-1 glyphicon glyphicon-play"></span> <div class="col-xs-11 name">'+job.name+'</div></h2></a>');
-	$(document).find('.running').append(html);
-};
-var addAbortedJob = function (job) {
-	var html = $.parseHTML('<a class="abort" href="'+job.url+'"><h2 class="col-xs-12 alert alert-warning"><span class="col-xs-1 glyphicon glyphicon-stop"></span> <div class="col-xs-11 name">'+job.name+'</div></h2></a>');
-	$(document).find('.aborted').append(html);
-};
-var addSeaOfGreen = function (job) {
-	var html = $.parseHTML('<h2 class="col-xs-12 alliswell alert alert-success"><span class="col-xs-1 glyphicon glyphicon-thumbs-up"></span> <div class="col-xs-11 name">Everything Is AWESOME!!!</div></h2>');
-	$(document).find('.okay').append(html);
-};
-var removeAllJobs = function (job) {
-	$(document).find('.problem').remove();
-	$(document).find('.run').remove();
-	$(document).find('.abort').remove();
-	$(document).find('.alliswell').remove();
-};
+	var seaOfGreen = _.template('<a class="job"><h2 class="col-xs-12 alliswell alert alert-success"><span class="col-xs-1 glyphicon glyphicon-thumbs-up"></span> <div class="col-xs-11 name">Everything Is AWESOME!!!</div></h2></a>');
 
-/* Useful helpers */
-var unwantedJobs = function (job) {
-	return _.contains(blacklistedJobs, job.name);
-};
-var currentlyRunning = function (job) {
-	return job.color.indexOf('_anime') > -1;
-};
+	var toProblem = function (job) {
+		return {
+			type: 'problem',
+			jobUrl: job.url,
+			alertState: 'danger',
+			iconCss: 'glyphicon glyphicon-thumbs-down',
+			jobName: job.name
+		};
+	};
 
-/* Download the current status of all jobs and display results */
+	var toRunning = function (job) {
+		return {
+			type: 'run',
+			jobUrl: job.url,
+			alertState: 'info',
+			iconCss: 'objrotate glyphicon glyphicon-refresh',
+			jobName: job.name
+		};
+	};
 
-var updateStatus = function () {
-	$.ajax({
-		dataType: 'jsonp',
-		url: 'http://it.linn.co.uk/hudson/api/json?jsonp=?',
-		success: function (data) {
-			var problemJobs = _.chain(data.jobs)
-				.where({'color': 'red'})
-				.reject(unwantedJobs)
-				.map(function (datum) { return { name: datum.name, url: datum.url }; })
-				.value();
-			var runningJobs = _.chain(data.jobs)
-				.filter(currentlyRunning)
-				.reject(unwantedJobs)
-				.map(function (datum) { return { name: datum.name, url: datum.url }; })
-				.value();
-			var abortedJobs = _.chain(data.jobs)
-				.where({'color': 'aborted'})
-				.reject(unwantedJobs)
-				.map(function (datum) { return { name: datum.name, url: datum.url }; })
-				.value();
-			removeAllJobs();
-			if (problemJobs.length + runningJobs.length + abortedJobs.length > 0) {
-				_.each(problemJobs, function (job) {
-					addProblemJob(job);
-				});
-				_.each(runningJobs, function (job) {
-					addRunningJob(job);
-				});
-				_.each(abortedJobs, function (job) {
-					addAbortedJob(job);
-				});
-			} else {
-				addSeaOfGreen();
+	var toAbort = function (job) {
+		return {
+			type: 'abort',
+			jobUrl: job.url,
+			alertState: 'warning',
+			iconCss: 'glyphicon glyphicon-stop',
+			jobName: job.name
+		};
+	};
+
+	var currentlyRunning = function (job) {
+		return job.color.indexOf('_anime') > -1;
+	};
+
+	var unwantedJobs = function (job) {
+		return _.contains(config.blacklistedJobs, job.name);
+	};
+
+	return function () {
+		$.ajax({
+			dataType: 'jsonp',
+			url: config.jenkinsRoot + 'api/json?jsonp=?',
+			success: function (data) {
+				var problemJobs = _.chain(data.jobs)
+					.where({'color': 'red'})
+					.reject(unwantedJobs)
+					.map(toProblem)
+					.value();
+				var runningJobs = _.chain(data.jobs)
+					.filter(currentlyRunning)
+					.reject(unwantedJobs)
+					.map(toRunning)
+					.value();
+				var abortedJobs = _.chain(data.jobs)
+					.where({'color': 'aborted'})
+					.reject(unwantedJobs)
+					.map(toAbort)
+					.value();
+				$(document).find('.job').remove();
+				var jobs = _.union(problemJobs, runningJobs, abortedJobs);
+				if (jobs.length > 0) {
+					_.each(jobs, function (job) {
+						var dom = jobTemplate(job);
+						$(document).find('.jobs').append(dom);
+					});
+				} else {
+					$(document).find('.jobs').append(seaOfGreen());
+				}
 			}
-		}
-	});
+		});
+	};
 };
+
+var config = {
+	"jenkinsRoot": "http://it.linn.co.uk/hudson/",
+	"blacklistedJobs": [ "LinnRecords (13) - Deploy All", "LinnRecords (14) - Nightly" ]
+};
+
+var updateStatus = init(config);
 updateStatus();
 setInterval(updateStatus, 10000);
